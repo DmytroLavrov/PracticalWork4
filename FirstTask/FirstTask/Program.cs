d220dfd5efda947f54aff269a97c05d73dcdeccb
DmytroLavrov <112572226+DmytroLavrov@users.noreply.github.com>
2023-03-17 21:26:50 +0200

    Add files via upload

---
+++ FirstTask/FirstTask/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace FirstTask
+{
+    class EventThrottler<T> where T : EventArgs
+    {
+        private object lockObj = new object();
+        private Queue<T> eventQueue = new Queue<T>();
+        private List<EventHandler<T>> eventHandlers = new List<EventHandler<T>>();
+        private int throttling;
+        public EventThrottler(int throttling)
+        {
+            throttling = throttling;
+            new Thread(EventThread).Start();
+        }
+
+        public void Register(EventHandler<T> handler)
+        {
+            lock (lockObj)
+            {
+                eventHandlers.Add(handler);
+            }
+        }
+
+        public void Unregister(EventHandler<T> handler)
+        {
+            lock (lockObj)
+            {
+                eventHandlers.Remove(handler);
+            }
+        }
+
+        public void Raise(object sender, T args)
+        {
+            lock (lockObj)
+            {
+                eventQueue.Enqueue(args);
+            }
+        }
+
+        private void EventThread()
+        {
+            while (true)
+            {
+                T[] eventsToProcess;
+
+                lock (lockObj)
+                {
+                    eventsToProcess = eventQueue.ToArray();
+                    eventQueue.Clear();
+                }
+
+                foreach (var e in eventsToProcess)
+                {
+                    lock (lockObj)
+                    {
+                        foreach (var handler in eventHandlers)
+                        {
+                            handler?.Invoke(this, e);
+                            Thread.Sleep(throttling);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var eventBus = new EventThrottler<EventArgs>(500);
+            EventHandler<EventArgs> handler1 = (sender, args) => Console.WriteLine("Handler1 called");
+            EventHandler<EventArgs> handler2 = (sender, args) => Console.WriteLine("Handler2 called");
+            EventHandler<EventArgs> handler3 = (sender, args) => Console.WriteLine("Handler3 called");
+            eventBus.Register(handler1);
+            eventBus.Register(handler2);
+            eventBus.Register(handler3);
+            eventBus.Raise(null, EventArgs.Empty);
+            Thread.Sleep(2000);
+            eventBus.Unregister(handler2);
+            eventBus.Raise(null, EventArgs.Empty);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SecondTask/SecondTask/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondTask
+{
+    class Publisher
+    {
+        public event EventHandler<Event> Handled;
+        public void PublishEvent(Event events)
+        {
+            Handled?.Invoke(this, events);
+        }
+    }
+    abstract class Subscriber
+    {
+        public abstract void Subscribe(object sender, Event events);
+    }
+    class PriorityHighSubscribe : Subscriber
+    {
+        public override void Subscribe(object sender, Event events)
+        {
+            if (events.Priority > 2)
+            {
+                Console.OutputEncoding = System.Text.Encoding.Default;
+                Console.WriteLine($"'{events.Name}' обробка із високим пріоритетом {nameof(PriorityHighSubscribe)}");
+            }
+        }
+    }
+    class PriorityMediumSubscribe : Subscriber
+    {
+        public override void Subscribe(object sender, Event events)
+        {
+            if (events.Priority == 2)
+            {
+                Console.OutputEncoding = System.Text.Encoding.Default;
+                Console.WriteLine($"'{events.Name}' обробка із середнім пріоритетом {nameof(PriorityMediumSubscribe)}");
+            }
+        }
+    }
+    class PriorityLowSubscribe : Subscriber
+    {
+        public override void Subscribe(object sender, Event events)
+        {
+            if (events.Priority < 2)
+            {
+                Console.OutputEncoding = System.Text.Encoding.Default;
+                Console.WriteLine($"'{events.Name}' обробка із низьким пріоритетом {nameof(PriorityLowSubscribe)}");
+            }
+        }
+    }
+    class Event : EventArgs
+    {
+        public int Priority { get; set; }
+        public string Name { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Publisher publisher = new Publisher();
+            Console.OutputEncoding = System.Text.Encoding.Default;
+            PriorityHighSubscribe priorityHighSubscribe = new PriorityHighSubscribe();
+            publisher.Handled += priorityHighSubscribe.Subscribe;
+            PriorityMediumSubscribe priorityMediumSubscribe = new PriorityMediumSubscribe();
+            publisher.Handled += priorityMediumSubscribe.Subscribe;
+            Event eventN1 = new Event();
+            eventN1.Name = "Подія N1";
+            eventN1.Priority = 3;
+            Event eventN2 = new Event();
+            eventN1.Name = "Подія N2";
+            eventN1.Priority = 2;
+            publisher.PublishEvent(eventN1);
+            publisher.PublishEvent(eventN2);
+            Console.ReadKey();
+        }
+    }
+}
